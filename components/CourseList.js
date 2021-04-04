7d50c8eeb2477a935011dfd6828854694276c99c
Ajuogaaz <brianlinus1753@gmail.com>
2021-04-04 18:05:35 -0500

    Course Conflicts

--- components/CourseList.js
+++ components/CourseList.js
@@ -2,13 +2,8 @@ import React,{ useState} from 'react';
 import { ScrollView, View, StyleSheet } from 'react-native'
 import Course from './Course'
 import TermSelector from './TermSelector'
-
-const termMap = {F : 'Fall', W : 'Winter', S : 'Spring'}
-const terms = Object.values(termMap);
-
-const getCourseTerm = course => (
-    termMap[course.id.charAt(0)]
-);
+import CourseSelector from  './CourseSelector'
+import {  getCourseTerm,  terms }from '../utils/course'
 
 const CourseList = ({ courses }) => {
 
@@ -16,29 +11,15 @@ const CourseList = ({ courses }) => {
 
     const termCourses  = courses.filter(course => selectedTerm === getCourseTerm(course))
 
+
     return (
-        <View>
+            
+        <ScrollView>
             <TermSelector terms={terms} selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
-            <ScrollView>
-                 <View style={styles.courseList}>
-                    { termCourses.map(course => <Course key={course.id} course={ course } />) }
-                </View>
-            </ScrollView>
-        </View>
-    
-    
+            <CourseSelector courses={termCourses}/>
+        </ScrollView>
   )};
 
 
-const styles = StyleSheet.create({
-    courseList: {
-      flex: 1,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-    },
-    
-  });  
 
 export default CourseList;
